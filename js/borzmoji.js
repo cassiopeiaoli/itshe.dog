82a988311a5b18eb611ed2149f6c4f508552b326
itchydoggie <cassiopeiaoliwa@gmail.com>
2025-04-20 22:01:44 +0200

    remove all old itshe.dog

--- js/borzmoji.js
+++
@@ -1,6 +0,0 @@
-const article = document.querySelector('.article');
-
-(() => {
-    article.innerHTML = article.innerHTML.replaceAll(':happy:', "<img class='emoji' src='" + '../assets/img/emojis/happy.png' + "'>");
-    article.innerHTML = article.innerHTML.replaceAll(':hmm:', "<img class='emoji' src='" + '../assets/img/emojis/hmm.png' + "'>");
-})();
