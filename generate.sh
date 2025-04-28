434137a9e4e064d8366d35204dc9539587afb7a0
itchydoggie <cassiopeiaoliwa@gmail.com>
2025-04-28 16:26:01 +0200

    added clicker to be a part of my website

---
+++ generate.sh
@@ -0,0 +1,6 @@
+# Uses global "clicker" software
+cd cassiopeia && clicker -i cassiopeia.dog -o cassiopeia.html
+cd ..
+cd ara && clicker -i ara.dog -o ara.html
+cd ..
+cd lyra && clicker -i lyra.dog -o lyra.html
