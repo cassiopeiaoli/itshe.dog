9696c0a59f9844f4b53b21f0b8685a449403c296
itchydoggie <cassiopeiaoliwa@gmail.com>
2025-04-28 22:06:23 +0200

    added buttons, removed pages for alters

--- generate.sh
+++
@@ -1,6 +0,0 @@
-# Uses global "clicker" software
-cd cassiopeia && clicker -i cassiopeia.dog -o cassiopeia.html
-cd ..
-cd ara && clicker -i ara.dog -o ara.html
-cd ..
-cd lyra && clicker -i lyra.dog -o lyra.html
